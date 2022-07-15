76a300d0be75cdb1c4983f36b080cde39e31bf07
Collins Kariuki <collinsmunkary@gmail.com>
2022-07-15 11:32:22 +0300

    Modify h1 upon click

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const button = document.querySelector('button');
+const h1 = document.querySelector('h1');
 button.addEventListener('click',function(){
    
     const r = Math.floor(Math.random() * 255);
@@ -7,6 +8,8 @@ button.addEventListener('click',function(){
 
     const newColor = `rgb(${r}, ${g}, ${b})`;
     document.body.style.backgroundColor = newColor;
+    h1.innerText = newColor;
+
 })
 
 
